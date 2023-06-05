2235fd23d36e1fc5885e434f4ea52766a6793b59
Julien Torrielli <jtorrielli@viaxoft.com>
2023-06-05 10:02:32 +0200

    init

---
+++ src/test/java/com/example/nativetest/NativeTestApplicationTests2.java
@@ -0,0 +1,13 @@
+package com.example.nativetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class NativeTestApplicationTests2 {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
